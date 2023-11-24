7330b12763dda365805fe25aeec9bb77ccb57fac
Yasha-21 <151942700+Yasha-21@users.noreply.github.com>
2023-11-24 20:01:28 +0000

    added function

--- main.c
+++ main.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
 #include <string.h>
-
+// function for login
+int login(char Username[], char Password[])
+{
+    // declare variables for test
+    char R_username[] = "admin";
+    char R_password[] = "admin";
+    // comparing user input with test
+    if(strcmp(Username, R_username) == 0 && strcmp(Password, R_password) == 0)
+        return 1; // successfull login 
+    else
+        return 0; // incorrect login
+}
 int main(void)
 {
-    //Usernames for test
-    char TestUsername[] = "user";
-    char TestPassword[] = "password123";
-    // Variables to store user input
+    // variables to store user input
     char InputName[20];
     char InputPassword[20];
-    //Printing message and geting user input
+    // printing message and geting user input
     printf("Enter your username: ");
     scanf("%s", InputName);
     printf("Enter your password: ");
     scanf("%s", InputPassword);
-    // Comparing user input to test username and password
-    if (strcmp(InputName, TestUsername) == 0 && strcmp(InputPassword, TestPassword) == 0)
-        printf("Logged in successfully. Welcome, %s!", InputName);
+    // calling function to check login
+    int LogginStatus = login(InputName, InputPassword);
+
+    // if login successfull print welcome message
+    if(LogginStatus)
+        printf("Login successfull. Welcome %s\n", InputName);
+    // if login unsuccessfull print error message
     else
-        printf("Incorrect username or password.");
+        printf("Error: Incorrect username or password\n");
     return 0;
 }
