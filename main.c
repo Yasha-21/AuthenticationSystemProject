c49d8238a011aa75fdc2958cf155f363f58371c6
Yasha-21 <151942700+Yasha-21@users.noreply.github.com>
2023-11-29 17:52:18 +0000

    version 0.4

--- main.c
+++ main.c
@@ -1,59 +1,54 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-// struct for binary tree
-struct node
-{
-  char data[20];
-  struct node* left;
-  struct node* right;
-};
-// inorder traversal
-void traversal(struct node* root)
-{
-  if (root == NULL) return;
-  traversal(root->left);
-  printf("%s ->", root->data);
-  traversal(root->right);
-}
-// creating a new Node
-struct node* newNode(char data[])
-{
-    // allocate memory for new node
-    struct node* node = (struct node*)malloc(sizeof(struct node));
-    // assign data to this node
-    strcpy(node->data, data);
-    // initialize left and right children as NULL
-    node->left = NULL;
-    node->right = NULL;
-    return (node);
-}
-struct node *insert(struct node *node, char data[])
-{
-  // return a new node if the tree is empty
-  if (node == NULL) return newNode(data);
-  // traverse to the right place and insert the node
-  if (data < node->data)
-    node->left = insert(node->left, data);
-  else
-    node->right = insert(node->right, data);
-  return node;
-}
 // function for login
-int login(char Username[], char Password[])
+int login()
 {
-    // declare variables for test
-    char R_username[] = "admin";
-    char R_password[] = "admin";
-    // comparing user input with test
-    if(strcmp(Username, R_username) == 0 && strcmp(Password, R_password) == 0)
-        return 1; // successfull login 
+    char Username[20];
+    char Password[20];
+    printf( "Enter Username: ");
+    scanf("%s", Username);
+    printf( "Enter Password: ");
+    scanf("%s", Password);
+    FILE *file = fopen("data.txt", "r");
+    char temp_username[20], temp_password[20];
+    if (file != NULL)
+    {
+        char line[20];
+        while (fgets(line, sizeof(line), file)) 
+        {
+            sscanf(line, "%s %s", temp_username, temp_password);
+        }
+        fclose(file);
+    } 
     else
-        return 0; // incorrect login
+        printf("Error opening the file.\n");
+    if (strcmp(Username, temp_username) == 0 && strcmp(Password, temp_password) == 0)
+        return 1;
+    else
+        return 0;
 }
+// function for registration
+void registration(char Username[], char Password[])
+{
+    FILE *file = fopen("data.txt", "a");
+    if (file != NULL) {
+        fputs(Username, file);
+        fputs("\n", file);
+        fputs(Password, file);
+        fputs("\n", file);
+        fclose(file);
+    } else {
+        printf("Error opening the file.\n");
+    }
+} 
 
 int main(void)
 {
-    
+    int answer = login();
+    if(answer)
+        printf("Welcome to the chatbot.\n");
+    else
+        printf("Invalid username or password.\n");
     return 0;
 }
